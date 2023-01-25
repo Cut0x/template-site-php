d8afb28a9c15e084deb98e731cf6fbf4c4ab933f
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-01-25 15:27:29 +0100

    Update config.php

--- Data/config.php
+++ Data/config.php
@@ -1,3 +1,16 @@
 <?php
     // Votre code ici
+
+    // Si vous voulez inclure une base de donnée
+    $db_host="host"; 
+    $db_user="username";
+    $db_password="password";   
+    $db_name="db_name";
+
+    try {
+	    $db=new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_password);
+	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOEXCEPTION $e) {
+	    $e->getMessage();
+    }
 ?>
